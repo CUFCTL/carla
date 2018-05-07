70cb184b96932e10b2102e3f092b65e5f4589efe
5lx <siriulxslx@gmail.com>
2018-05-07 00:48:53 -0400

    Change Image augment

--- FCTL_imitation_learning_train/train.py
+++ FCTL_imitation_learning_train/train.py
@@ -19,8 +19,8 @@ rl = lambda aug: iaa.Sometimes(0.09, aug)
 seq = iaa.Sequential([
     rl(iaa.GaussianBlur((0, 1.5))),  # blur images with a sigma between 0 and 1.5
     rl(iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05), per_channel=0.5)),  # add gaussian noise to images
-    oc(iaa.Dropout((0.0, 0.10), per_channel=0.5)),  # randomly remove up to X% of the pixels
-    oc(iaa.CoarseDropout((0.0, 0.10), size_percent=(0.08, 0.2), per_channel=0.5)),
+    # oc(iaa.Dropout((0.0, 0.10), per_channel=0.5)),  # randomly remove up to X% of the pixels
+    # oc(iaa.CoarseDropout((0.0, 0.10), size_percent=(0.08, 0.2), per_channel=0.5)),
     # randomly remove up to X% of the pixels
     oc(iaa.Add((-40, 40), per_channel=0.5)),  # change brightness of images (by -X to Y of original value)
     st(iaa.Multiply((0.10, 2.5), per_channel=0.2)),  # change brightness of images (X-Y% of original value)
@@ -39,7 +39,7 @@ trainScratch = True
 
 # Configurations
 num_images = 657800
-itername = 80000
+itername = 300000
 memory_fraction = 0.25
 image_cut = [115, 510]
 dropoutVec = [1.0] * 8 + [0.7] * 2 + [0.5] * 2 + [0.5] * 1 + [0.5, 1.] * 2
